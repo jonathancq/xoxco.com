baeadf985e73e3dc0c41dd4cc6d9ca673dd9eb9c
Ben Brown <ben@xoxco.com>
2013-06-01 11:40:16 -0500

    add animated logo to project pages

--- js/main.js
+++ js/main.js
@@ -2,6 +2,28 @@ $(function() {
 
 	// enable Aware.js, and add new flags to any sort of post-like content	
  	$('.tile').aware();
- 	
+	var delay = 500;
+	$('ul#animated_logo li').each(function() {
+		(function(el) {
+			setTimeout(function() { $(el).css('top','20px'); },delay);
+		})(this);							
+		delay = delay + 100;
+	
+	});
+	
+	setTimeout(function() { 
+
+			$('ul#animated_logo li').css('-webkit-transition-duration','0.1s');
+			$('ul#animated_logo').on('mouseenter','li',function() {
+				$(this).css('top','0px');
+				(function(el) {
+					setTimeout(function() { $(el).css('top','20px'); },100);
+				})(this)		
+
+			});
+
+	
+	
+	},delay); 	
 	
 });
